e4bc7e04806283fa54c6b9387618d9907fb615a2
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-02-28 16:56:43 +0700

    now

--- bot.php
+++ bot.php
@@ -60,67 +60,105 @@ $content = file_get_contents('php://input');
 // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
 $events = json_decode($content, true);
 $events = json_decode($content, true);
-if(!is_null($events)){
+if (!is_null($events)) {
     // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
     $replyToken = $events['events'][0]['replyToken'];
     $typeMessage = $events['events'][0]['message']['type'];
     $userMessage = $events['events'][0]['message']['text'];
     $userMessage = strtolower($userMessage);
-    switch ($typeMessage){
+    switch ($typeMessage) {
         case 'text':
             switch ($userMessage) {
                 case "m":
                     $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
-                    $textMessage = new TextMessageBuilder($textReplyMessage);                
-                                     
+                    $textMessage = new TextMessageBuilder($textReplyMessage);
+
                     $placeName = "ที่ตั้งร้าน";
                     $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
                     $latitude = 13.780401863217657;
                     $longitude = 100.61141967773438;
-                    $locationMessage = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);        
-                 
+                    $locationMessage = new LocationMessageBuilder($placeName, $placeAddress, $latitude, $longitude);
+
                     $multiMessage = new MultiMessageBuilder;
                     $multiMessage->add($textMessage);
                     $multiMessage->add($imageMessage);
                     $multiMessage->add($locationMessage);
-                    $replyData = $multiMessage;                                     
-                    break;              
-                               
-                case "tm":
-                    $replyData = new TemplateMessageBuilder('Confirm Template',
-                        new ConfirmTemplateBuilder(
-                                'Confirm template builder',
-                                array(
-                                    new MessageTemplateActionBuilder(
-                                        'Yes',
-                                        'Text Yes'
-                                    ),
-                                    new MessageTemplateActionBuilder(
-                                        'No',
-                                        'Text NO'
-                                    ),
-                                    new MessageTemplateActionBuilder(
-                                        'No',
-                                        'Text NO'
-                                    )
-                                )
+                    $replyData = $multiMessage;
+                    break;
+                case "t_b":
+                    // กำหนด action 4 ปุ่ม 4 ประเภท
+                    $actionBuilder = array(
+                        new MessageTemplateActionBuilder(
+                            'Message Template', // ข้อความแสดงในปุ่ม
+                            'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                        ),
+                        new UriTemplateActionBuilder(
+                            'Uri Template', // ข้อความแสดงในปุ่ม
+                            'https://www.ninenik.com'
+                        ),
+                        new DatetimePickerTemplateActionBuilder(
+                            'Datetime Picker', // ข้อความแสดงในปุ่ม
+                            http_build_query(array(
+                                'action' => 'reservation',
+                                'person' => 5
+                            )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                            'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
+                            substr_replace(date("Y-m-d H:i"), 'T', 10, 1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
+                            substr_replace(date("Y-m-d H:i", strtotime("+5 day")), 'T', 10, 1), //วันที่ เวลา มากสุดที่เลือกได้
+                            substr_replace(date("Y-m-d H:i"), 'T', 10, 1) //วันที่ เวลา น้อยสุดที่เลือกได้
+                        ),
+                        new PostbackTemplateActionBuilder(
+                            'Postback', // ข้อความแสดงในปุ่ม
+                            http_build_query(array(
+                                'action' => 'buy',
+                                'item' => 100
+                            )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                            'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                        ),
+                    );
+                    $imageUrl = 'https://www.mywebsite.com/imgsrc/photos/w/simpleflower';
+                    $replyData = new TemplateMessageBuilder(
+                        'Button Template',
+                        new ButtonTemplateBuilder(
+                            'button template builder', // กำหนดหัวเรื่อง
+                            'Please select', // กำหนดรายละเอียด
+                            $imageUrl, // กำหนด url รุปภาพ
+                            $actionBuilder  // กำหนด action object
                         )
                     );
-                    break;                                                                                                                          
+                    break;
+                case "tm":
+                    $replyData = new TemplateMessageBuilder(
+                        'Confirm Template',
+                        new ConfirmTemplateBuilder(
+                            'Confirm template builder',
+                            array(
+                                new MessageTemplateActionBuilder(
+                                    'Yes',
+                                    'Text Yes'
+                                ),
+                                new MessageTemplateActionBuilder(
+                                    'No',
+                                    'Text NO'
+                                )
+                            )
+                        )
+                    );
+                    break;
                 default:
                     $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
-                    $replyData = new TextMessageBuilder($textReplyMessage);         
-                    break;                                      
+                    $replyData = new TextMessageBuilder($textReplyMessage);
+                    break;
             }
             break;
         default:
             $textReplyMessage = json_encode($events);
-            $replyData = new TextMessageBuilder($textReplyMessage);         
-            break;  
+            $replyData = new TextMessageBuilder($textReplyMessage);
+            break;
     }
 }
 //l ส่วนของคำสั่งตอบกลับข้อความ
-$response = $bot->replyMessage($replyToken,$replyData);
+$response = $bot->replyMessage($replyToken, $replyData);
 
 // Failed
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
