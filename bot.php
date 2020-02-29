4b15c27585a6964901fdebf0facad45f66a4ee3d
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-02-29 11:32:05 +0700

    now

--- bot.php
+++ bot.php
@@ -194,7 +194,7 @@ if (!is_null($events)) {
                             "vertical",
                             array(
                                 new ButtonComponentBuilder(
-                                    new UriTemplateActionBuilder("Primary style button","กรอกชื่อผู้ใช้"),
+                                    new MessageTemplateActionBuilder("Primary style button","กรอกชื่อผู้ใช้"),
                                     NULL,NULL,NULL,"primary"
                                 ),
                                 new ButtonComponentBuilder(
