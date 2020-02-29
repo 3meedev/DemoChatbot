7b6fd24e9885ef69078cc88e2051a7ad8a7a60db
Thanakrit <thanakrit.sa.61@ubu.ac.th>
2020-02-29 12:05:13 +0700

    now

--- bot.php
+++ bot.php
@@ -67,106 +67,6 @@ use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
 
 // ----------------------------------------------------------------------------------------------------- แบบ Template Message
 
-// $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
-// $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
-
-
-// $content = file_get_contents('php://input');
-
-
-
-// $events = json_decode($content, true);
-// if (!is_null($events)) {
-    
-//     $replyToken = $events['events'][0]['replyToken'];
-//     $typeMessage = $events['events'][0]['message']['type'];
-//     $userMessage = $events['events'][0]['message']['text'];
-//     $userMessage = strtolower($userMessage);
-//     switch ($typeMessage) {
-//         case 'text':
-//             switch ($userMessage) {
-//                 case "ตอบกลับ":
-//                     $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
-//                     $textMessage = new TextMessageBuilder($textReplyMessage);
-//                     break;
-//                 case "โลเคชั่น":
-//                     $placeName = "ที่ตั้งร้าน";
-//                     $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
-//                     $latitude = 13.780401863217657;
-//                     $longitude = 100.61141967773438;
-//                     $locationMessage = new LocationMessageBuilder($placeName, $placeAddress, $latitude, $longitude);
-//                     break;
-//                 case "แจ้งปัญหา":                   
-//                     $actionBuilder = array(
-//                         new MessageTemplateActionBuilder(
-//                             'ลืมรหัสผ่าน',
-//                             'กรอกชื่อผู้ใช้ 1'
-//                         ),
-//                         new MessageTemplateActionBuilder(
-//                             'ยอดเงินไม่เข้า',
-//                             'กรอกชื่อผู้ใช้ 2'
-//                         ),
-//                         new UriTemplateActionBuilder(
-//                             'รายละเอียดเพิ่มเติม',
-//                             'https://www.google.com/?hl=th'
-//                         ),   
-//                         new MessageTemplateActionBuilder(
-//                             'อื่นๆ',
-//                             'กรอกชื่อผู้ใช้ 3'
-//                         ),
-                                          
-//                     );
-//                     $imageUrl = 'https://i2.wp.com/sagaming168.com/wp-content/uploads/2018/12/sa-game-casino.jpg?resize=578%2C337&ssl=1';
-//                     $replyData = new TemplateMessageBuilder(
-//                         'แจ้งปัญหา',
-//                         new ButtonTemplateBuilder(
-//                             'แจ้งปัญหา',
-//                             'กรุณาเลือกหัวข้อที่ต้องการ',
-//                             $imageUrl,
-//                             $actionBuilder 
-//                         )
-//                     );
-//                     break;
-//                 case "ติดต่อ":
-//                     $replyData = new TemplateMessageBuilder(
-//                         'Confirm Template',
-//                         new ConfirmTemplateBuilder(
-//                             'Confirm template builder',
-//                             array(
-//                                 new MessageTemplateActionBuilder(
-//                                     'Yes',
-//                                     'Text Yes'
-//                                 ),
-//                                 new MessageTemplateActionBuilder(
-//                                     'No',
-//                                     'Text NO'
-//                                 )
-//                             )
-//                         )
-//                     );
-//                     break;
-//                 default:
-//                     $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
-//                     $replyData = new TextMessageBuilder($textReplyMessage);
-//                     break;
-//             }
-//             break;
-//         default:
-//             $textReplyMessage = json_encode($events);
-//             $replyData = new TextMessageBuilder($textReplyMessage);
-//             break;
-//     }
-// }
-
-// $response = $bot->replyMessage($replyToken, $replyData);
-
-
-// echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-// ----------------------------------------------------------------------------------------------------------------------------
-
-// -------------------------------------------------------------------------------------------------------------------------- แบบ Flex Message
-
 $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
 $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
 
@@ -184,35 +84,62 @@ if (!is_null($events)) {
     $userMessage = strtolower($userMessage);
     switch ($typeMessage) {
         case 'text':
-            switch ($userMessage) {                
-                
-                case "ติดต่อ":
-                    $textReplyMessage = new BubbleContainerBuilder(
-                        "ltr",  // กำหนด NULL หรือ "ltr" หรือ "rtl"
-                        NULL,NULL,
-                        new ImageComponentBuilder(
-                            "https://i2.wp.com/sagaming168.com/wp-content/uploads/2018/12/sa-game-casino.jpg?resize=578%2C337&ssl=1",NULL,NULL,NULL,NULL,"full","20:13","cover"),
-                        new BoxComponentBuilder(
-                            "vertical",
-                            array(
-                                new ButtonComponentBuilder(
-                                    new UriTemplateActionBuilder("Primary style button","http://niik.in"),
-                                    NULL,NULL,NULL,"primary"
-                                ),
-                                new ButtonComponentBuilder(
-                                    new UriTemplateActionBuilder("Secondary  style button","http://niik.in"),
-                                    NULL,NULL,NULL,"secondary"
-                                ),          
-                                new ButtonComponentBuilder(
-                                    new UriTemplateActionBuilder("Link  style button","http://niik.in"),
-                                    NULL,NULL,NULL,"link"
-                                ),                                  
-                            ),
-                            0,"md"
-                        )
+            switch ($userMessage) {
+                case "ตอบกลับ":
+                    $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+                    $textMessage = new TextMessageBuilder($textReplyMessage);
+                    break;
+                case "โลเคชั่น":
+                    $placeName = "ที่ตั้งร้าน";
+                    $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
+                    $latitude = 13.780401863217657;
+                    $longitude = 100.61141967773438;
+                    $locationMessage = new LocationMessageBuilder($placeName, $placeAddress, $latitude, $longitude);
+                    break;
+                case "แจ้งปัญหา":                   
+                    $actionBuilder = array(
+                        new ButtonComponentBuilder(
+                            new UriTemplateActionBuilder("Primary style button","http://niik.in"),
+                            NULL,NULL,NULL,"primary"
+                        ),
+                        new ButtonComponentBuilder(
+                            new UriTemplateActionBuilder("Secondary  style button","http://niik.in"),
+                            NULL,NULL,NULL,"secondary"
+                        ),          
+                        new ButtonComponentBuilder(
+                            new UriTemplateActionBuilder("Link  style button","http://niik.in"),
+                            NULL,NULL,NULL,"link"
+                        ),                                  
+                        0,"md"                        
                     );      
-             
-            $replyData = new FlexMessageBuilder("Flex",$textReplyMessage);
+                    $imageUrl = 'https://i2.wp.com/sagaming168.com/wp-content/uploads/2018/12/sa-game-casino.jpg?resize=578%2C337&ssl=1';
+                    $replyData = new TemplateMessageBuilder(
+                        'แจ้งปัญหา',
+                        new ButtonTemplateBuilder(
+                            'แจ้งปัญหา',
+                            'กรุณาเลือกหัวข้อที่ต้องการ',
+                            $imageUrl,
+                            $actionBuilder 
+                        )
+                    );
+                    break;
+                case "ติดต่อ":
+                    $replyData = new TemplateMessageBuilder(
+                        'Confirm Template',
+                        new ConfirmTemplateBuilder(
+                            'Confirm template builder',
+                            array(
+                                new MessageTemplateActionBuilder(
+                                    'Yes',
+                                    'Text Yes'
+                                ),
+                                new MessageTemplateActionBuilder(
+                                    'No',
+                                    'Text NO'
+                                )
+                            )
+                        )
+                    );
                     break;
                 default:
                     $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
@@ -221,17 +148,9 @@ if (!is_null($events)) {
             }
             break;
         default:
-        $textReplyMessage = new BubbleContainerBuilder(
-            "ltr",NULL,NULL,
-            new BoxComponentBuilder(
-                "vertical",
-                array(
-                    new TextComponentBuilder("hello"),
-                    new TextComponentBuilder("world")
-                )
-            )
-        );
-        $replyData = new FlexMessageBuilder("This is a Flex Message",$textReplyMessage);
+            $textReplyMessage = json_encode($events);
+            $replyData = new TextMessageBuilder($textReplyMessage);
+            break;
     }
 }
 
@@ -241,4 +160,81 @@ $response = $bot->replyMessage($replyToken, $replyData);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 // ----------------------------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------------------------- แบบ Flex Message
+
+// $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+// $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+
+
+// $content = file_get_contents('php://input');
+
+
+
+// $events = json_decode($content, true);
+// if (!is_null($events)) {
+    
+//     $replyToken = $events['events'][0]['replyToken'];
+//     $typeMessage = $events['events'][0]['message']['type'];
+//     $userMessage = $events['events'][0]['message']['text'];
+//     $userMessage = strtolower($userMessage);
+//     switch ($typeMessage) {
+//         case 'text':
+//             switch ($userMessage) {                
+                
+//                 case "ติดต่อ":
+//                     $textReplyMessage = new BubbleContainerBuilder(
+//                         "ltr",  // กำหนด NULL หรือ "ltr" หรือ "rtl"
+//                         NULL,NULL,
+//                         new ImageComponentBuilder(
+//                             "https://i2.wp.com/sagaming168.com/wp-content/uploads/2018/12/sa-game-casino.jpg?resize=578%2C337&ssl=1",NULL,NULL,NULL,NULL,"full","20:13","cover"),
+//                         new BoxComponentBuilder(
+//                             "vertical",
+//                             array(
+//                                 new ButtonComponentBuilder(
+//                                     new UriTemplateActionBuilder("Primary style button","http://niik.in"),
+//                                     NULL,NULL,NULL,"primary"
+//                                 ),
+//                                 new ButtonComponentBuilder(
+//                                     new UriTemplateActionBuilder("Secondary  style button","http://niik.in"),
+//                                     NULL,NULL,NULL,"secondary"
+//                                 ),          
+//                                 new ButtonComponentBuilder(
+//                                     new UriTemplateActionBuilder("Link  style button","http://niik.in"),
+//                                     NULL,NULL,NULL,"link"
+//                                 ),                                  
+//                             ),
+//                             0,"md"
+//                         )
+//                     );      
+             
+//             $replyData = new FlexMessageBuilder("Flex",$textReplyMessage);
+//                     break;
+//                 default:
+//                     $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
+//                     $replyData = new TextMessageBuilder($textReplyMessage);
+//                     break;
+//             }
+//             break;
+//         default:
+//         $textReplyMessage = new BubbleContainerBuilder(
+//             "ltr",NULL,NULL,
+//             new BoxComponentBuilder(
+//                 "vertical",
+//                 array(
+//                     new TextComponentBuilder("hello"),
+//                     new TextComponentBuilder("world")
+//                 )
+//             )
+//         );
+//         $replyData = new FlexMessageBuilder("This is a Flex Message",$textReplyMessage);
+//     }
+// }
+
+// $response = $bot->replyMessage($replyToken, $replyData);
+
+
+// echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+// // ----------------------------------------------------------------------------------------------------------------------------
 ?>
